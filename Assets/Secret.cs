ea21770fa70c0ab5e7052d78d79f23498ce49ee3
Ukoidyost <44127839+Ukoidyost@users.noreply.github.com>
2019-02-01 13:31:00 -0500

    Twitch Interface
    assets for unity project

---
+++ Assets/Secret.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Secret 
+{
+
+    public static string client_id = "hsmqipkpjo6nbfr8512e0bm1dnoxft";
+    public static string client_secret = "2sbjy87qspgj47nvyubefai6tfwb2w";
+    public static string bot_access_token = "4sedkkjbrewx6uu3pbn590334uijuh";
+    public static string bot_refresh_token = "js313wmbzvi7f3cuto2rkskbn2zlhf42odizbu3f7r985dwrp4";
+
+}
