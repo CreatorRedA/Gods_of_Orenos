0303f80320ac253fb8c6f1359d2e2670940387e9
Noah <37704804+NoahYB@users.noreply.github.com>
2019-02-03 14:48:49 -0500

    Add files via upload
    Assets folder for tuning variables scripts

---
+++ Assets/Scripts/menuNav.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuNav : MonoBehaviour
+{
+    public void goToGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void Start()
+    {
+
+    }
+}
