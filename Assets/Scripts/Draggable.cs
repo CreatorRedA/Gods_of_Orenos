827f3353f4b359cb19a8fa9bc4fad1f90ae0b947
Noah <37704804+NoahYB@users.noreply.github.com>
2019-02-05 18:46:23 -0500

    Add files via upload
    Assets for card game UI set up and webgl build

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Draggable : MonoBehaviour, IDragHandler , IBeginDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        parentToReturnTo = this.transform.parent;
+
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+
+        this.transform.SetParent(parentToReturnTo);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
